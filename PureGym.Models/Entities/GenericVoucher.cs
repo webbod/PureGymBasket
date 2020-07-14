6ac407ae62e036de254b8ca660c6e1a0e5e37a48
Graeme Mulvaney <web.bod@gmail.com>
2020-07-14 07:45:50 +0100

    Replaced cost with value - vouchers can be applied to baskets

--- PureGym.Models/Entities/GenericVoucher.cs
+++ PureGym.Models/Entities/GenericVoucher.cs
@@ -1,41 +1,25 @@
 ﻿using PureGym.Common;
-using PureGym.Common.Exceptions;
 using PureGym.Interfaces.Common;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PureGym.Models.Entities
 {
     public class GenericVoucher : AGenericEntity, IIsAVoucherItem
     {
-        public string Description { get; private set; }
-
-        public Money Value { get; private set; }
-
         public bool Applied { get; private set; }
 
         public GenericVoucher() {   }
 
-        public GenericVoucher(string key, string description, Money cost, Guid id = default(Guid)) :
-            base(key, id)
+        public GenericVoucher(string key, string description, Money value, Guid id = default(Guid)) : base()
         {
-            Init(key, description, cost, id);
+            Init(key, description, value, id);
         }
 
-        protected void Init(string key, string description, Money value, Guid id = default(Guid))
-        {
-            if (HasBeenInitalised()) { return; }
+        public void ApplyVoucher() { Applied = true; }
 
-            if (value < 0) { throw new PriceOutOfRangeException($"{nameof(value)} {SharedStrings.WasNegative}"); }
-            Value = value;
+        public void DontApplyVoucher() { Applied = false; }
 
-            if (string.IsNullOrEmpty(description)) { throw new ArgumentNullException($"{nameof(description)} {SharedStrings.WasEmpty}"); }
-            Description = description;
-
-            Applied = false;
-
-            base.Init(key, id);
-        }
+        public override string ToString() => $"{Description} {(Applied ? SharedStrings.Applied : "")}";
+        
     }
 }
