5eaf2f9220d43a0d548090f25def8f33b89b55f6
Graeme Mulvaney <web.bod@gmail.com>
2020-07-14 07:50:25 +0100

    Renamed PriceOutOfRangeException

--- PureGym.Common/Exceptions/ValueOutOfRangeException.cs
+++ PureGym.Common/Exceptions/ValueOutOfRangeException.cs
@@ -5,9 +5,9 @@ namespace PureGym.Common.Exceptions
     /// <summary>
     /// The exception that is thrown when an item has an invalid price
     /// </summary>
-    public class PriceOutOfRangeException : ArgumentOutOfRangeException
+    public class ValueOutOfRangeException : ArgumentOutOfRangeException
     {
-        public PriceOutOfRangeException(string message) : base(message)
+        public ValueOutOfRangeException(string message) : base(message)
         {
 
         }
