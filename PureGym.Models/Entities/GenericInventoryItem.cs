b3a464c849dfd69c1e22d78f0b7877f41a0ff2d9
Graeme Mulvaney <web.bod@gmail.com>
2020-07-14 07:46:54 +0100

    replaced price with value and tidied up

--- PureGym.Models/Entities/GenericInventoryItem.cs
+++ PureGym.Models/Entities/GenericInventoryItem.cs
@@ -1,11 +1,8 @@
 ﻿using Newtonsoft.Json;
 using PureGym.Common;
 using PureGym.Common.Enumerations;
-using PureGym.Common.Exceptions;
 using PureGym.Interfaces.Common;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PureGym.Models.Entities
 {
@@ -13,37 +10,26 @@ namespace PureGym.Models.Entities
     public class GenericInventoryItem : AGenericEntity, IIsAnInventoryItem, IHasAValue
     {
         [JsonProperty]
-        public string Description { get; private set; }
-
-        [JsonProperty]
-        public Money Price { get; private set; }
-
-        [JsonProperty]
         public StockCategory Category { get; private set; }
 
         public GenericInventoryItem() : base()
         {
         }
 
-        public GenericInventoryItem(string key, string description, Money price, StockCategory category, Guid id = default(Guid)) :
+        public GenericInventoryItem(string key, string description, Money value, StockCategory category, Guid id = default(Guid)) :
             base()
         {
-            Init(key, description, price, category, id);
+            Init(key, description, value, category, id);
         }
 
-        protected void Init(string key, string description, Money price, StockCategory category, Guid id = default(Guid))
+        protected void Init(string key, string description, Money value, StockCategory category, Guid id = default(Guid))
         {
-            if (HasBeenInitalised()) { return; }
+            if (HasBeenInitalised) { return; }
 
-            if (price.Value < 0) { throw new PriceOutOfRangeException($"{nameof(price)} {SharedStrings.WasNegative}"); }
-            Price = price;
-
-            if (string.IsNullOrEmpty(description)) { throw new ArgumentNullException($"{nameof(description)} {SharedStrings.WasEmpty}"); }
-            Description = description;
-
+            Helper.CheckIfValueIsNotNull(category);
             Category = category;
 
-            base.Init(key, id);
+            base.Init(key, description, value, id);
         }
     }
 }
