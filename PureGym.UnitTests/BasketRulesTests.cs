ef4fa9191f672afbefe3e3a462f11b83ceed498b
Graeme Mulvaney <web.bod@gmail.com>
2020-07-14 12:05:13 +0100

    Added Unit Tests for BasketRules

---
+++ PureGym.UnitTests/BasketRulesTests.cs
@@ -0,0 +1,71 @@
+﻿using PureGym.Common;
+using PureGym.Common.Enumerations;
+using PureGym.Common.Exceptions;
+using PureGym.Models.Containers;
+using PureGym.Models.Entities;
+using PureGym.Models.Rules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PureGym.UnitTests
+{
+    public class BasketRulesTests
+    {
+        private BasketContainer<GenericBasketItem> newBasket(){
+            var basket = new BasketContainer<GenericBasketItem>(Currency.GBP);
+            basket.Insert(new GenericBasketItem("T01", "footwear", new Money(80, Currency.GBP), StockCategory.Footwear, 1));
+            basket.Insert(new GenericBasketItem("T02", "shoes", new Money(30, Currency.GBP), StockCategory.Vouchers, 1));
+            return basket;
+        }
+
+        [Fact]
+        public void ThisRuleIsValidIfBasketContainsTheRightStuff()
+        {
+            var basket = newBasket();
+            var actual = basket.GetAll()
+                .IsNotEmpty()
+                .AndHasStuffFrom(StockCategory.Footwear)
+                .ThenItIsValid();
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void ThisRuleIsValidIfBasketHasAHighEnoughValue()
+        {
+            var basket = newBasket();
+            var actual = basket.GetAll()
+                .IsNotEmpty()
+                .AndTheValueIsAtLeast(new Money(100, Currency.GBP))
+                .ThenItIsValid();
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void ThisRuleIsValidIfBasketHasAHighEnoughValueAfterIgnoringSomeStuff()
+        {
+            var basket = newBasket();
+            var actual = basket.GetAll()
+                .IsNotEmpty()
+                .AndIfWeIgnoreStuffFrom(StockCategory.Vouchers)
+                .AndTheValueIsAtLeast(new Money(70, Currency.GBP))
+                .ThenItIsValid();
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void ThisRuleIsNotValidBecaueBasketValueIsTooLowAfterIgnoringSomeStuff()
+        {
+            var basket = newBasket();
+            Assert.Throws<OfferNotValidException>(() =>
+                basket.GetAll()
+                .IsNotEmpty()
+                .AndIfWeIgnoreStuffFrom(StockCategory.Footwear)
+                .AndTheValueIsAtLeast(new Money(70, Currency.GBP))
+                .ThenItIsValid()
+            );
+        }
+    }
+}
